478eaba5b6690be0fea43106bbea9ca92bb546fb
Diogo Souza <bdiogo212@gmail.com>
2022-02-04 21:53:12 -0300

    llllllll

--- app/Http/Requests/UpdateTodoRequest.php
+++ app/Http/Requests/UpdateTodoRequest.php
@@ -13,7 +13,7 @@ class UpdateTodoRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return $this->todo->user_id === $this->user()->id;
     }
 
     /**
